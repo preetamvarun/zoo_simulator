a854db34b12c57b3c206a4b614bb9b8e58344ee2
preetam <preetamvarun99@gmail.com>
2024-02-23 15:07:51 +0000

    added comments to animalsHealthSlice

--- src/utils/animalsHealthSlice.js
+++ src/utils/animalsHealthSlice.js
@@ -3,11 +3,13 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const animalsHealthSlice =  createSlice({
     name : "animalHealthSlice",
+    /* Initially, healths of each animal will be at 100% */
     initialState : {
         animalsHealth : [[100,100,100,100,100], [100,100,100,100,100], [100,100,100,100,100]],
     },
     reducers : {
         updateAnimalsHealth : (state, action) => {
+            /* we will get an updated 2D array from action.payload */
             state.animalsHealth = action.payload
         }
     }
