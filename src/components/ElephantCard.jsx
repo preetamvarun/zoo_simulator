faa6fd7958f4a1d584d34cab53d6a13ed1284713
preetam <preetamvarun99@gmail.com>
2024-02-24 01:56:48 +0000

    animated feeding to elephants as well

--- src/components/ElephantCard.jsx
+++ src/components/ElephantCard.jsx
@@ -3,12 +3,16 @@ import React, { useEffect } from 'react';
 import ElephantHealthy from '../Images/ElephantHealthy.svg';
 import {useDispatch, useSelector} from 'react-redux';
 import { setElephantStatus } from '../utils/elephantStatusSlice';
+import Food from '../Images/Food.svg';
+import '../styles/AnimateFood.css';
 
 /* Get the health of the current elephant and keep track of current elephant with the help of index */
 const ElephantCard = ({health, index}) => {
 
   /* subscribe to the elephant status slice from the store. */
   const elephantStatusSlice = useSelector((store) => store.elephantStatus)
+
+  const animalsFeedStatus = useSelector((store) => store.animalsHealth).areAnimalsFed
 
   const dispatch = useDispatch()
 
@@ -21,7 +25,7 @@ const ElephantCard = ({health, index}) => {
   const currentElephantStatus = elephantStatusSlice.elephantsStatus[index].status;
 
   return (
-    <div className='shadow-md font-mono text-sm w-56'>
+    <div className='relative shadow-md font-mono text-sm w-56 overflow-hidden'>
 
       {/* Elephant Image Goes In Here */}
       <div className='overflow-hidden opacity-95 hover:opacity-100 hover:cursor-pointer duration-200 flex flex-row items-center justify-center'>
@@ -33,6 +37,9 @@ const ElephantCard = ({health, index}) => {
         {currentElephantStatus !== "Dead" && <p> Health : {health}%</p>}
         <p> Status : {currentElephantStatus}</p>
       </div>
+
+      <img src= {Food} className={`absolute top-0 left-[1/2] w-10/12 translate-y-1/2 -translate-x-2/3
+      ${animalsFeedStatus && 'animate-feed'}`}/>
 
     </div>
   )
