7000099074d27930d6d3f880eed07f8106502004
preetam <preetamvarun99@gmail.com>
2024-02-25 16:12:05 +0000

    added time

--- src/components/MainShelter.jsx
+++ src/components/MainShelter.jsx
@@ -90,9 +90,7 @@ const MainShelter = () => {
                     <button onClick={handleProvoke} className="px-6 py-1 text-white"
                     >Provoke</button>
                 </div>
-                <div className="inline-block text-white">
-                    <p>{currentTime.toLocaleTimeString()}</p>
-                </div>
+                <p className="inline-block text-[#FFD700] text-xl font-jungle-fever text-center"> TIME : {currentTime.toLocaleTimeString()}</p>
                 <div className="inline-block border-2 border-solid border-green-400 px-[12px] py-[2px]
                 Btn-Feed"
                 >
